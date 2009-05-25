a3c197a63d7d90b9f24de23e8bca8de6acb16a99
aalmiray <aalmiray@82b81fbe-1ee3-11de-bf1e-7b5f7763e386>
2009-05-25 20:26:59 +0000

    chap08: added java/groovy examples, upgraded ThreadedFileFiewer to griffon 0.1.1
    git-svn-id: http://griffoninaction.googlecode.com/svn/trunk@6 82b81fbe-1ee3-11de-bf1e-7b5f7763e386

--- chap08/ThreadingFileViewer/griffon-app/views/ThreadingFileViewerView.groovy
+++ chap08/ThreadingFileViewer/griffon-app/views/ThreadingFileViewerView.groovy
@@ -9,7 +9,11 @@ actions {
 }
 
 fileChooserWindow = fileChooser()
-fileViewerWindow = application(title:'ThreadingFileViewer', size:[480,320], locationByPlatform:true) {
+fileViewerWindow = application(title:'ThreadingFileViewer', size:[480,320], locationByPlatform:true,
+  iconImage: imageIcon('/griffon-icon-48x48.png').image,
+  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
+               imageIcon('/griffon-icon-32x32.png').image,
+               imageIcon('/griffon-icon-16x16.png').image]) {
    menuBar {
       menu("File") {
          menuItem(quitAction)
