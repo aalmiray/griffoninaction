7ff531f3dd9478a9d4549f79e92b0ccdd99d03b1
aalmiray <aalmiray@82b81fbe-1ee3-11de-bf1e-7b5f7763e386>
2009-07-25 21:43:18 +0000

    updated code to Griffon 0.1.2
    git-svn-id: http://griffoninaction.googlecode.com/svn/trunk@9 82b81fbe-1ee3-11de-bf1e-7b5f7763e386

--- chap02/GroovyEdit/griffon-app/views/GroovyEditView.groovy
+++ chap02/GroovyEdit/griffon-app/views/GroovyEditView.groovy
@@ -12,7 +12,11 @@ actions {
 }
 
 fileChooserWindow = fileChooser()
-fileViewerWindow = application(title:'GroovyEdit', size:[480,320], locationByPlatform:true) {
+fileViewerWindow = application(title:'GroovyEdit', size:[480,320], locationByPlatform:true,
+  iconImage: imageIcon('/griffon-icon-48x48.png').image,
+  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
+               imageIcon('/griffon-icon-32x32.png').image,
+               imageIcon('/griffon-icon-16x16.png').image] ) {
    menuBar {
       menu("File") {
          menuItem(openAction)
